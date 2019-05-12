4ce5220ee8ee6c7759c41625678820993ec603c1
m-luketin <ml46019@oss.unist.hr>
2019-05-12 21:44:16 +0200

    Implemented IRepo interface on repos

--- Backend/Normalization.Repository/Repositories/AttributeCollectionRepository.cs
+++ Backend/Normalization.Repository/Repositories/AttributeCollectionRepository.cs
@@ -4,16 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Normalization.Data.Contexts;
-
+using Normalization.Data.Models;
+using Normalization.Repository.Interfaces;
 namespace Normalization.Repository.Repositories
 {
-    public class AttributeCollectionRepository
+    public class AttributeCollectionRepository : IRepository
     {
         private readonly AttributeCollectionContext _attributeCollectionContext;
 
-        public AttributeCollectionRepository()
+        public AttributeCollectionRepository() 
         {
             _attributeCollectionContext = new AttributeCollectionContext();
+        }
+
+        public void Create(IEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(IEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(IEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEntity GetById(int id)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- Backend/Normalization.Repository/Repositories/TableAttributeCollectionRepository.cs
+++ Backend/Normalization.Repository/Repositories/TableAttributeCollectionRepository.cs
@@ -4,16 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Normalization.Data.Contexts;
+using Normalization.Data.Models;
+using Normalization.Repository.Interfaces;
 
 namespace Normalization.Repository.Repositories
 {
-    public class TableAttributeCollectionRepository
+    public class TableAttributeCollectionRepository : IRepository
     {
         private readonly TableAttributeCollectionContext _tableAttributeCollectionContext;
 
         public TableAttributeCollectionRepository()
         {
             _tableAttributeCollectionContext = new TableAttributeCollectionContext();
+        }
+
+        public void Create(IEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(IEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(IEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEntity GetById(int id)
+        {
+            throw new NotImplementedException();
         }
     }
 }
