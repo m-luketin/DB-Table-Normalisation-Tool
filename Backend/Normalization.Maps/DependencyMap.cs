cf007cd17791551ceadf6199c37287814902beaa
AnteVuletic <antevuletic1995@gmail.com>
2019-05-19 19:43:08 +0200

    Staging maps

--- Backend/Normalization.Maps/DependencyMap.cs
+++ Backend/Normalization.Maps/DependencyMap.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Normalization.ViewModel;
 
 namespace Normalization.Maps
 {
-    class DependencyMap
+    public class DependencyMap : IMaps
     {
+        public IViewModel Create(IViewModel item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IViewModel Update(IViewModel item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IViewModel ReadFromId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<IViewModel> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
