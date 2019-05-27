a2182c017cc2585f9f3be5480e52a3b36ff76d79
m-luketin <ml46019@oss.unist.hr>
2019-05-27 22:48:43 +0200

    Implemented algorithm

---
+++ Backend/Normalization.Maps/Algorithm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Normalization.ViewModel;
+
+namespace Normalization.Maps.Algorithm
+{
+    public class Algorithm
+    {
+        public NormalizedViewModel NormalizeTable(TableViewModel table)
+        {
+            var normalizedTable = new NormalizedViewModel();
+            var decompositionElement = new List<string>();
+            
+            foreach (var dependencyViewModel in table.Dependencies)
+            {
+                decompositionElement.AddRange(dependencyViewModel.From);
+                decompositionElement.Add(dependencyViewModel.To);
+                normalizedTable.TableAttributes.Add(decompositionElement);
+                decompositionElement.Clear();
+            }
+
+            decompositionElement.AddRange(table.Keys.First());
+            normalizedTable.TableAttributes.Add(decompositionElement);
+            decompositionElement.Clear();
+
+            normalizedTable.TableAttributes = normalizedTable.TableAttributes.Distinct().ToList();
+
+
+            return normalizedTable;
+        }
+    }
+}
