e6cadde617523a2ccd5a72dc70f62fdb84a351ba
m-luketin <ml46019@oss.unist.hr>
2019-05-27 23:19:38 +0200

    Forgot a file. Tired.

---
+++ Backend/Normalization.ViewModel/NormalizedViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Normalization.ViewModel
+{
+    public class NormalizedViewModel
+    {
+        public string SchemaName { get; set; }
+        public ICollection<ICollection<string>> TableAttributes { get; set; }
+
+        public NormalizedViewModel()
+        {
+
+        }
+        public NormalizedViewModel(string name, ICollection<ICollection<string>> attributes)
+        {
+            SchemaName = name;
+            TableAttributes = attributes;
+        }
+    }
+}
